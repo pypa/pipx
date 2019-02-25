ced00a15cf00dbb66b075d929205eff5172aa175
Laurențiu Andronache <laurentiu.andronache@trailung.ro>
2019-02-25 22:34:56 +0000

    remove unused pragma

--- pipx/venv_metadata_inspector.py
+++ pipx/venv_metadata_inspector.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-from pathlib import Path
-import sys
 import json
+import sys
+from pathlib import Path
 from typing import Dict, List, Optional
 
 
