b479a8285ee101a2efacb135e6ee36edce1dccdb
Paul Moore <p.f.moore@gmail.com>
2019-06-01 14:09:20 +0100

    Support existing venvs that don't use the shared libs

--- pipx/constants.py
+++ pipx/constants.py
@@ -9,6 +9,7 @@ DEFAULT_PIPX_BIN_DIR = Path.home() / ".local/bin"
 PIPX_HOME = Path(os.environ.get("PIPX_HOME", DEFAULT_PIPX_HOME)).resolve()
 PIPX_LOCAL_VENVS = PIPX_HOME / "venvs"
 PIPX_SHARED_LIBS = PIPX_HOME / "shared"
+PIPX_SHARED_PTH = "pipx_shared.pth"
 LOCAL_BIN_DIR = Path(os.environ.get("PIPX_BIN_DIR", DEFAULT_PIPX_BIN_DIR)).resolve()
 PIPX_VENV_CACHEDIR = PIPX_HOME / ".cache"
 PIPX_PACKAGE_NAME = "pipx"
