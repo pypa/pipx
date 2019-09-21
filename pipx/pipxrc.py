8045dde125fd1a8efbe67d688c48ac03c54d7c3f
Matthew Clapp <itsayellow+dev@gmail.com>
2019-09-20 18:36:50 -0700

    Rename JsonPathEncoder -> JsonEncoderPipx, ser changes.
    Update serialization/deserialization code to standardize
    special objects to be all converted to
    {'__type__':<obj_type>, '__<obj_type>__':<serialized_obj>}
    format.

--- pipx/pipxrc.py
+++ pipx/pipxrc.py
@@ -4,8 +4,9 @@ from pathlib import Path
 from pipx.Venv import PipxVenvMetadata
 
 
-class JsonPathEncoder(json.JSONEncoder):
+class JsonEncoderPipx(json.JSONEncoder):
     def default(self, obj):
+        # only handles what json.JSONEncoder doesn't understand by default
         if isinstance(obj, Path):
             return {'__type__':'Path', '__Path__':str(obj)}
         return super().default(self, obj)
@@ -14,16 +15,23 @@ class JsonPathEncoder(json.JSONEncoder):
 def _json_decoder_object_hook(json_dict):
     if json_dict.get('__type__', None) == 'Path' and '__Path__' in json_dict:
         return Path(json_dict['__Path__'])
+    if json_dict.get('__type__', None) == 'PipxVenvMetadata' and '__PipxVenvMetadata__' in json_dict:
+        return PipxVenvMetadata(**json_dict['__PipxVenvMetadata__'])
     return json_dict
 
 
 def write_pipxrc(venv_dir: Path, pipxrc_info: dict):
-    if 'venv_metadata' in pipxrc_info:
-        # serialize PipxVenvMetadata
-        pipxrc_info['venv_metadata'] = dict(pipxrc_info['venv_metadata']._asdict())
+    # json thinks PipxVenvMetadata is just another tuple, so we override here
+    #   (JSONEncoder override is harder and messier.)
+    for key in pipxrc_info:
+        if isinstance(pipxrc_info[key], PipxVenvMetadata):
+            pipxrc_info[key] = {
+                    '__type__':'PipxVenvMetadata',
+                    '__PipxVenvMetadata__': dict(pipxrc_info[key]._asdict())
+                    }
     # TODO 20190919: raise exception on failure?
     with open(venv_dir / 'pipxrc', 'w') as pipxrc_fh:
-        json.dump(pipxrc_info, pipxrc_fh, cls=JsonPathEncoder)
+        json.dump(pipxrc_info, pipxrc_fh, cls=JsonEncoderPipx)
 
 
 def read_pipxrc(venv_dir: Path) -> dict:
@@ -33,10 +41,5 @@ def read_pipxrc(venv_dir: Path) -> dict:
     except IOError:
         # return empty dict if no pipxrc file or unreadable
         return {}
-    # convert venv_data back to NamedTuple
-    if 'venv_metadata' in pipxrc_info:
-        pipxrc_info['venv_metadata'] = PipxVenvMetadata(**pipxrc_info['venv_metadata'])
 
     return pipxrc_info
-
-
