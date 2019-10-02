5f632b7cd346412bb2bc5f99d8b1dd8b6e0faa52
Matthew Clapp <itsayellow+dev@gmail.com>
2019-10-01 19:29:33 -0700

    Clean up of _abs_path_if_local().  Add comments.
    Also add check for --editable in pip_args, which indicates
    not a pypi package for sure.

--- src/pipx/commands.py
+++ src/pipx/commands.py
@@ -345,32 +345,43 @@ def install(
 
 
 def _abs_path_if_local(package_or_url: str, venv: Venv, pip_args: List[str]) -> str:
-    # if not pip --editable, then pip assumes any one-word word spec MUST
-    #   be a pypi package
+    # TODO 20191001 if not pip --editable, then pip assumes any one-word word
+    #   spec MUST be a pypi package?
+
     pkg_path = Path(package_or_url)
     if not pkg_path.exists():
         # no existing path, must be pypi package or non-existent
         return package_or_url
 
+    # Editable packages are either local or url, non-url must be local.
+    if "--editable" in pip_args and pkg_path.exists():
+        return str(pkg_path.resolve())
+
+    # https://www.python.org/dev/peps/pep-0508/#names
     valid_pkg_name = bool(
         re.search(r"^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$", package_or_url, re.I)
     )
-
     if not valid_pkg_name:
         return str(pkg_path.resolve())
 
+    # If all of the above conditions do not return, we may have used a pypi
+    #   package.
+    # If we find a pypi package with this name installed, assume we just
+    #   installed it.
     pip_search_args: List[str]
 
+    # If user-defined pypi index url, then use it for search
     try:
         arg_i = pip_args.index("--index-url")
     except ValueError:
         pip_search_args = []
     else:
         pip_search_args = pip_args[arg_i : arg_i + 2]
-    pip_search_result_str = venv.pip_search(package_or_url, pip_search_args)
 
+    pip_search_result_str = venv.pip_search(package_or_url, pip_search_args)
     pip_search_results = pip_search_result_str.split("\n")
 
+    # Get package_or_url and following related lines from pip search stdout
     pkg_found = False
     pip_search_found = []
     for pip_search_line in pip_search_results:
@@ -382,12 +393,9 @@ def _abs_path_if_local(package_or_url: str, venv: Venv, pip_args: List[str]) -> 
         elif pip_search_line.startswith(package_or_url):
             pip_search_found.append(pip_search_line)
             pkg_found = True
+    pip_found_str = " ".join(pip_search_found)
 
-    if (
-        len(pip_search_found) > 1
-        and pip_search_found[0].startswith(package_or_url)
-        and "INSTALLED" in pip_search_found[-1]
-    ):
+    if pip_found_str.startswith(package_or_url) and "INSTALLED" in pip_found_str:
         return package_or_url
     else:
         return str(pkg_path.resolve())
