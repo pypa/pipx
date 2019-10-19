eb1113ea4566c0959736f37353e940ed88032da6
Matthew Clapp <itsayellow+dev@gmail.com>
2019-10-19 10:33:12 -0700

    Change in-line type to type--Python 3.5 compatibility.

--- pipx/venv_metadata_inspector.py
+++ pipx/venv_metadata_inspector.py
@@ -86,7 +86,7 @@ def main():
 
     apps = get_apps(package, bin_path)
     app_paths = [str(Path(bin_path) / app) for app in apps]
-    app_paths_of_dependencies: Dict[str, List[str]] = {}
+    app_paths_of_dependencies = {}  # type: Dict[str, List[str]]
     app_paths_of_dependencies = _dfs_package_apps(
         bin_path, package, app_paths_of_dependencies
     )
