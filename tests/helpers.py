0c0f1c2782a3557fee6197cb00594845290f8dc3
Matthew Clapp <itsayellow+dev@gmail.com>
2019-10-29 16:30:45 -0700

    Black reformat.

--- tests/helpers.py
+++ tests/helpers.py
@@ -20,13 +20,16 @@ def run_pipx_cli(pipx_args: List[str]):
 def which_python(python_exe):
     try:
         pyenv_which = subprocess.run(
-            ["pyenv", "which", python_exe], stdout=subprocess.PIPE, universal_newlines=True
+            ["pyenv", "which", python_exe],
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
         )
     except FileNotFoundError:
         # no pyenv on system
         return which(python_exe)
 
-    if pyenv_which.returncode==0:
+    if pyenv_which.returncode == 0:
         return pyenv_which.stdout.strip()
     else:
+        # pyenv on system, but pyenv has no path to python_exe
         return None
