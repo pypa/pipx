d78c014ae795aa1cafc77204713e02efad25d653
Matthew Clapp <itsayellow+dev@gmail.com>
2019-10-29 10:02:28 -0700

    Make which_python pyenv compatible.

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,3 +1,5 @@
+from shutil import which
+import subprocess
 import sys
 from typing import List
 from unittest import mock
@@ -13,3 +15,12 @@ def assert_not_in_virtualenv():
 def run_pipx_cli(pipx_args: List[str]):
     with mock.patch.object(sys, "argv", ["pipx"] + pipx_args):
         return main.cli()
+
+
+def which_python(python_exe):
+    pyenv_which = subprocess.run(["pyenv", "which", python_exe], stdout=subprocess.PIPE)
+    if not pyenv_which.returncode:
+        python_path = pyenv_which.stdout.decode().strip()
+    else:
+        python_path = which(python_exe)
+    return python_path
