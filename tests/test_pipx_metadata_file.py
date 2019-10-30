d61fa620933281066c7671f8f72ff7e7f82f8e50
Matthew Clapp <itsayellow+dev@gmail.com>
2019-10-29 19:06:07 -0700

    Start of test_pipx_metadata_file.py.

---
+++ tests/test_pipx_metadata_file.py
@@ -0,0 +1,10 @@
+from pipx.pipx_metadata_file import PipxMetadata, PackageInfo
+
+
+# test to make sure we never have duplicate injected packages
+#   this might happen during reinstall_all if we don't properly uninstall
+#       injected packages or don't remove their metadata
+
+
+def test_pipx_metadata_file_create():
+    assert False
