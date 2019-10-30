a86d0dd97ed3f5f2106cc919833297b7e40eca3d
Matthew Clapp <itsayellow+dev@gmail.com>
2019-10-30 16:19:03 -0700

    Add mock PackageInfo to test.

--- tests/test_pipx_metadata_file.py
+++ tests/test_pipx_metadata_file.py
@@ -9,4 +9,15 @@ from pipx.pipx_metadata_file import PipxMetadata, PackageInfo
 def test_pipx_metadata_file_create(tmp_path):
     venv_temp = tmp_path / "venv1"
     pipx_metadata = PipxMetadata(venv_temp)
-    # assert False
+    package_info = PackageInfo(
+        package="test_package",
+        package_or_url="test_package_url",
+        pip_args=[],
+        include_apps=True,
+        include_dependencies=True,
+        apps=[],
+        app_paths=[],
+        apps_of_dependencies=[],
+        app_paths_of_dependencies=[],
+        package_version="0.0.0",
+    )
