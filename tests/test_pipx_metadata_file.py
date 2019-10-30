12c82a7bcd190334261353e03b89038c4a0e3600
Matthew Clapp <itsayellow+dev@gmail.com>
2019-10-30 14:40:45 -0700

    Add temp_path fixture.

--- tests/test_pipx_metadata_file.py
+++ tests/test_pipx_metadata_file.py
@@ -6,6 +6,7 @@ from pipx.pipx_metadata_file import PipxMetadata, PackageInfo
 #       injected packages or don't remove their metadata
 
 
-def test_pipx_metadata_file_create():
-    pass
-    #assert False
+def test_pipx_metadata_file_create(tmp_path):
+    venv_temp = tmp_path / "venv1"
+    pipx_metadata = PipxMetadata(venv_temp)
+    # assert False
