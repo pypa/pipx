bdedd7e0fed408f52a1c97a3be4ca61fd96251c4
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-04 14:38:26 -0800

    Change reinstall-all to have optional --python arg. (#228)
    * Change test_reinstall_all to use optional --python Instead of positional required python argument.
    * Fix help text for reinstall-all.
    
    This makes reinstall-all consistent with install and run,
    subcommands which both use --python as their optional switch
    for specifying the python interpreter location, and uses
    the same default of DEFAULT_PYTHON as those subcommands.
    
    This makes reinstall-all much more useful to use after a
    system python upgrade--instead of hunting for the proper
    location of the new system python, the user can just execute
    'pipx reinstall-all' and the system should find the proper
    new upgraded python executable.

--- src/pipx/main.py
+++ src/pipx/main.py
@@ -392,10 +392,10 @@ def _add_reinstall_all(subparsers):
     p = subparsers.add_parser(
         "reinstall-all",
         formatter_class=LineWrapRawTextHelpFormatter,
-        help="Reinstall all packages with a different Python executable",
+        help="Reinstall all packages",
         description=textwrap.dedent(
             """
-        Reinstalls all packages using a different version of Python.
+        Reinstalls all packages.
 
         Packages are uninstalled, then installed with pipx install PACKAGE.
         This is useful if you upgraded to a new version of Python and want
@@ -407,7 +407,11 @@ def _add_reinstall_all(subparsers):
         """
         ),
     )
-    p.add_argument("python")
+    p.add_argument(
+        "--python",
+        default=constants.DEFAULT_PYTHON,
+        help="The Python version to reinstall the package's CLI app with. Must be v3.5+.",
+    )
     add_include_dependencies(p)
     add_pip_venv_args(p)
     p.add_argument("--skip", nargs="+", default=[], help="skip these packages")
