c5ccfd53c89e6851d5020e98830af4566b59a8d5
Chris Hunt <chrahunt@gmail.com>
2019-12-14 09:43:20 -0500

    Use _expose_apps_globally from commands.common
    The content in both files is the same, per
    
    diff <(sed -n '12,78p' common.py) <(sed -n '522,588p' commands.py)

--- src/pipx/commands/commands.py
+++ src/pipx/commands/commands.py
@@ -4,7 +4,6 @@ import datetime
 import hashlib
 import logging
 import shlex
-import shutil
 import subprocess
 import sys
 import textwrap
@@ -31,13 +30,13 @@ else:
 import userpath  # type: ignore
 from pipx import constants
 from pipx.colors import bold, red
+from pipx.commands.common import _expose_apps_globally
 from pipx.constants import TEMP_VENV_EXPIRATION_THRESHOLD_DAYS
 from pipx.emojies import hazard, sleep, stars
 from pipx.util import (
     WINDOWS,
     PipxError,
     get_pypackage_bin_path,
-    mkdir,
     rmdir,
     run_pypackage_bin,
 )
@@ -516,75 +515,6 @@ def reinstall_all(
                 include_apps=injected_package.include_apps,
                 include_dependencies=injected_package.include_dependencies,
                 force=True,
-            )
-
-
-def _expose_apps_globally(
-    local_bin_dir: Path, app_paths: List[Path], package: str, *, force: bool
-):
-    if WINDOWS:
-        _copy_package_apps(local_bin_dir, app_paths, package)
-    else:
-        _symlink_package_apps(local_bin_dir, app_paths, package, force=force)
-
-
-def _copy_package_apps(local_bin_dir: Path, app_paths: List[Path], package: str):
-    for src_unresolved in app_paths:
-        src = src_unresolved.resolve()
-        app = src.name
-        dest = Path(local_bin_dir / app)
-        if not dest.parent.is_dir():
-            mkdir(dest.parent)
-        if dest.exists():
-            logging.warning(f"{hazard}  Overwriting file {str(dest)} with {str(src)}")
-            dest.unlink()
-        if src.exists():
-            shutil.copy(src, dest)
-
-
-def _symlink_package_apps(
-    local_bin_dir: Path, app_paths: List[Path], package: str, *, force: bool
-):
-    for app_path in app_paths:
-        app_name = app_path.name
-        symlink_path = Path(local_bin_dir / app_name)
-        if not symlink_path.parent.is_dir():
-            mkdir(symlink_path.parent)
-
-        if force:
-            logging.info(f"Force is true. Removing {str(symlink_path)}.")
-            try:
-                symlink_path.unlink()
-            except FileNotFoundError:
-                pass
-            except IsADirectoryError:
-                rmdir(symlink_path)
-
-        exists = symlink_path.exists()
-        is_symlink = symlink_path.is_symlink()
-        if exists:
-            if symlink_path.samefile(app_path):
-                logging.info(f"Same path {str(symlink_path)} and {str(app_path)}")
-            else:
-                logging.warning(
-                    f"{hazard}  File exists at {str(symlink_path)} and points "
-                    f"to {symlink_path.resolve()}, not {str(app_path)}. Not modifying."
-                )
-            continue
-        if is_symlink and not exists:
-            logging.info(
-                f"Removing existing symlink {str(symlink_path)} since it "
-                "pointed non-existent location"
-            )
-            symlink_path.unlink()
-
-        existing_executable_on_path = which(app_name)
-        symlink_path.symlink_to(app_path)
-
-        if existing_executable_on_path:
-            logging.warning(
-                f"{hazard}  Note: {app_name} was already on your PATH at "
-                f"{existing_executable_on_path}"
             )
 
 
