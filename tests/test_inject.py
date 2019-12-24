4e9547ef7802a94b991d25e2f83154517a1d9b4b
Matthew Clapp <itsayellow+dev@gmail.com>
2019-12-23 23:33:29 -0500

    Add test of inject with package spec.

--- tests/test_inject.py
+++ tests/test_inject.py
@@ -7,6 +7,11 @@ def test_simple(pipx_temp_env, capsys):
     assert not run_pipx_cli(["inject", "pycowsay", "black"])
 
 
+def test_spec(pipx_temp_env, capsys):
+    assert not run_pipx_cli(["install", "pycowsay"])
+    assert not run_pipx_cli(["inject", "pycowsay", "pylint==2.3.1"])
+
+
 def test_include_apps(pipx_temp_env, capsys):
     assert not run_pipx_cli(["install", "pycowsay"])
     assert run_pipx_cli(["inject", "pycowsay", "black", "--include-deps"])
