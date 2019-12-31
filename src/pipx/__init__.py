74c2b859e6c11f1f9f58a9e657f03d7c029ab5a5
Chad Smith <cs01@users.noreply.github.com>
2019-12-31 15:28:30 -0800

    exit instead of raising assertion error

--- src/pipx/__init__.py
+++ src/pipx/__init__.py
@@ -1,6 +1,7 @@
 import sys
 
-assert sys.version_info >= (3, 6, 0), (
-    "Python 3.6+ is required. See https://github.com/pipxproject/pipx "
-    "for installation instructions"
-)
+if sys.version_info < (3, 6, 0):
+    exit(
+        "Python 3.6+ is required. See https://github.com/pipxproject/pipx "
+        "for installation instructions."
+    )
