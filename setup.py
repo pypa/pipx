1a80da5debd8a059a255fbad48d5722b7d76e043
Matthew Clapp <itsayellow+dev@gmail.com>
2020-01-05 11:43:10 -0800

    Add Python 3.8 to classifiers.

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
     ],
     project_urls={
