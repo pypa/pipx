0af6812684334a99703b6fc3be3addb4e45e2b04
Keith Maxwell <keith.maxwell@gmail.com>
2020-02-18 01:09:23 +0000

    Remove dependency on packaging library (#367)

--- setup.py
+++ setup.py
@@ -13,25 +13,23 @@ if sys.version_info < (3, 6, 0):
 import io  # noqa E402
 import os  # noqa E402
 from pathlib import Path  # noqa E402
+from runpy import run_path  # noqa E402
 from typing import List  # noqa E402
 import ast  # noqa E402
 import re  # noqa E402
 
 CURDIR = Path(__file__).parent
 
-REQUIRED = ["userpath", "argcomplete>=1.9.4, <2.0", "packaging"]  # type: List[str]
+REQUIRED = ["userpath", "argcomplete>=1.9.4, <2.0"]  # type: List[str]
 
 with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
     README = f.read()
 
 
 def get_version():
-    main_file = CURDIR / "src" / "pipx" / "main.py"
-    _version_re = re.compile(r"__version__\s+=\s+(?P<version>.*)")
-    with open(main_file, "r", encoding="utf8") as f:
-        match = _version_re.search(f.read())
-        version = match.group("version") if match is not None else '"unknown"'
-    return str(ast.literal_eval(version))
+    version_file = CURDIR / "src" / "pipx" / "version.py"
+    namespace = run_path(version_file)
+    return namespace["__version__"]
 
 
 setup(
