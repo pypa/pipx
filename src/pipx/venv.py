fa98846ced0870327aa069213bfb29c2c4b49d16
Matthew Clapp <itsayellow+dev@gmail.com>
2020-03-05 19:55:34 -0800

    Change error message to omit "Pipx".

--- src/pipx/venv.py
+++ src/pipx/venv.py
@@ -245,7 +245,7 @@ class Venv:
 
         venv_metadata_traceback = data.pop("exception_traceback", None)
         if venv_metadata_traceback is not None:
-            logging.error("Pipx internal error with venv metadata inspection.")
+            logging.error("Internal error with venv metadata inspection.")
             logging.info(
                 f"venv_metadata_inspector.py Traceback:\n{venv_metadata_traceback}"
             )
