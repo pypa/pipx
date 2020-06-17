6d83e3b3d913a7c0a4b76fd18343af2697dedbf5
Matthew Clapp <itsayellow+dev@gmail.com>
2020-06-16 17:32:39 -0700

    Remove obsolete valid_pypi_name() in util.py.

--- src/pipx/util.py
+++ src/pipx/util.py
@@ -135,13 +135,6 @@ def run(cmd: Sequence[Union[str, Path]], check=True) -> int:
     return returncode
 
 
-def valid_pypi_name(package_name: str) -> bool:
-    # https://www.python.org/dev/peps/pep-0508/#names
-    return bool(
-        re.search(r"^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$", package_name, re.I)
-    )
-
-
 def full_package_description(package: str, package_spec: str) -> str:
     if package == package_spec:
         return package
