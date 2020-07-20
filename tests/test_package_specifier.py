17a7243c9688a7ff2ac2da3f8bfed26afbbf8d5f
Matthew Clapp <itsayellow+dev@gmail.com>
2020-07-19 18:43:54 -0700

    Initial version of tests for parse_specifier.

---
+++ tests/test_package_specifier.py
@@ -0,0 +1,59 @@
+import pytest  # type: ignore
+
+from pipx.package_specifier import parse_specifier
+from pipx.util import PipxError
+
+# TODO: Make sure git+ works with tests, correct in test_install as well
+@pytest.mark.parametrize(
+    "package_spec_in,package_or_url_correct,valid_spec",
+    [
+        ("pipx", "pipx", True),
+        ("pipx==0.15.0", "pipx", True),
+        ("pipx>=0.15.0", "pipx", True),
+        ("pipx<=0.15.0", "pipx", True),
+        ('pipx;python_version>="3.6"', "pipx", True),
+        ('pipx==0.15.0;python_version>="3.6"', "pipx", True),
+        (
+            "git+https://github.com/cs01/nox.git@5ea70723e9e6",
+            "git+https://github.com/cs01/nox.git@5ea70723e9e6",
+            True,
+        ),
+        (
+            "nox@git+https://github.com/cs01/nox.git@5ea70723e9e6",
+            "git+https://github.com/cs01/nox.git@5ea70723e9e6",
+            True,
+        ),
+        (
+            "https://github.com/ambv/black/archive/18.9b0.zip",
+            "https://github.com/ambv/black/archive/18.9b0.zip",
+            True,
+        ),
+        (
+            "black@https://github.com/ambv/black/archive/18.9b0.zip",
+            "https://github.com/ambv/black/archive/18.9b0.zip",
+            True,
+        ),
+        (
+            "black @ https://github.com/ambv/black/archive/18.9b0.zip",
+            "https://github.com/ambv/black/archive/18.9b0.zip",
+            True,
+        ),
+        (
+            "path/doesnt/exist",
+            "NA",
+            False,
+        ),
+        (
+            "https:/github.com/ambv/black/archive/18.9b0.zip",
+            "NA",
+            False,
+        ),
+    ],
+)
+def test_parse_specifier(package_spec_in, package_or_url_correct, valid_spec):
+    if valid_spec:
+        package_or_url = parse_specifier(package_spec_in)
+        assert package_or_url == package_or_url_correct
+    else:
+        with pytest.raises(PipxError):
+            package_or_url = parse_specifier(package_spec_in)
