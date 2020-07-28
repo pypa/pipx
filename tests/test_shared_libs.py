0068f9ab73170eb75ab2c58e494475bb75ded39f
Matthew Clapp <itsayellow+dev@gmail.com>
2020-07-27 19:38:53 -0700

    Compute now in test function to prevent error due to long test.

--- tests/test_shared_libs.py
+++ tests/test_shared_libs.py
@@ -4,21 +4,19 @@ import pytest  # type: ignore
 from pipx import shared_libs
 
 
-now = time.time()
-
-
 @pytest.mark.parametrize(
-    "mtime,needs_upgrade",
+    "mtime_minus_now,needs_upgrade",
     [
-        (now - shared_libs.SHARED_LIBS_MAX_AGE_SEC - 20 * 60, True),
-        (now - shared_libs.SHARED_LIBS_MAX_AGE_SEC + 20 * 60, False),
+        (-shared_libs.SHARED_LIBS_MAX_AGE_SEC - 5 * 60, True),
+        (-shared_libs.SHARED_LIBS_MAX_AGE_SEC + 5 * 60, False),
     ],
 )
-def test_auto_update_shared_libs(capsys, pipx_temp_env, mtime, needs_upgrade):
+def test_auto_update_shared_libs(capsys, pipx_temp_env, mtime_minus_now, needs_upgrade):
+    now = time.time()
     shared_libs.shared_libs.create([], verbose=True)
     shared_libs.shared_libs.has_been_updated_this_run = False
 
     access_time = now  # this can be anything
-    os.utime(shared_libs.shared_libs.pip_path, (access_time, mtime))
+    os.utime(shared_libs.shared_libs.pip_path, (access_time, mtime_minus_now + now))
 
     assert shared_libs.shared_libs.needs_upgrade is needs_upgrade
