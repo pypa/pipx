531d3c2a03751a4f36f4f257eca1b7358ffe982b
Matthew Clapp <itsayellow+dev@gmail.com>
2020-08-05 20:20:43 -0700

    More install tests.

--- tests/test_package_specifier.py
+++ tests/test_package_specifier.py
@@ -2,7 +2,10 @@ from pathlib import Path
 
 import pytest  # type: ignore
 
-from pipx.package_specifier import parse_specifier_for_metadata
+from pipx.package_specifier import (
+    parse_specifier_for_install,
+    parse_specifier_for_metadata,
+)
 from pipx.util import PipxError
 
 
@@ -67,26 +70,57 @@ def test_parse_specifier_for_metadata(
 
 
 @pytest.mark.parametrize(
-    "package_spec_in,pip_args_in,package_spec_expected,pip_args_expected",
+    "package_spec_in,pip_args_in,package_spec_expected,pip_args_expected,warning_str",
     [
-        ('pipx==0.15.0;python_version>="3.6"', [], "pipx==0.15.0", []),
-        ('pipx==0.15.0;python_version>="3.6"', ["--editable"], "pipx==0.15.0", []),
+        ('pipx==0.15.0;python_version>="3.6"', [], "pipx==0.15.0", [], None),
+        ("pipx==0.15.0", ["--editable"], "pipx==0.15.0", [], "Ignoring --editable",),
+        (
+            'pipx==0.15.0;python_version>="3.6"',
+            [],
+            "pipx==0.15.0",
+            [],
+            "Ignoring environment markers",
+        ),
+        (
+            "pipx==0.15.0",
+            ["--no-cache-dir", "--editable"],
+            "pipx==0.15.0",
+            ["--no-cache-dir"],
+            "Ignoring --editable",
+        ),
         (
             "git+https://github.com/cs01/nox.git@5ea70723e9e6",
             ["--editable"],
             "git+https://github.com/cs01/nox.git@5ea70723e9e6",
             [],
+            "Ignoring --editable",
         ),
         (
             "https://github.com/ambv/black/archive/18.9b0.zip",
             ["--editable"],
             "https://github.com/ambv/black/archive/18.9b0.zip",
             [],
+            "Ignoring --editable",
         ),
-        ("src/pipx", ["--editable"], str(Path("src/pipx").resolve()), ["--editable"]),
+        (
+            "src/pipx",
+            ["--editable"],
+            str(Path("src/pipx").resolve()),
+            ["--editable"],
+            None,
+        ),
     ],
 )
 def test_parse_specifier_for_install(
-    package_spec_in, pip_args_in, package_spec_expected, pip_args_expected
+    caplog,
+    package_spec_in,
+    pip_args_in,
+    package_spec_expected,
+    pip_args_expected,
+    warning_str,
 ):
-    pass
+    [package_or_url_out, pip_args_out] = parse_specifier_for_install(
+        package_spec_in, pip_args_in
+    )
+    if warning_str is not None:
+        assert warning_str in caplog.text
