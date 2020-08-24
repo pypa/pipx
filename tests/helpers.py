10e92764eb5667a6861ef368a26d732603c83d07
Jon <jonringer@users.noreply.github.com>
2020-08-24 12:01:06 -0700

    Remove venv check

--- tests/helpers.py
+++ tests/helpers.py
@@ -9,7 +9,6 @@ from pipx import main
 
 def assert_not_in_virtualenv():
     assert not hasattr(sys, "real_prefix"), "Tests cannot run under virtualenv"
-    assert getattr(sys, "base_prefix", sys.prefix) != sys.prefix, "Tests require venv"
 
 
 def run_pipx_cli(pipx_args: List[str]):
