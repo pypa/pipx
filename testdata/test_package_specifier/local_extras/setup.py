ebbb9e17f4d6c2bc3cd5b5814916dd1fce53d591
Matthew Clapp <itsayellow+dev@gmail.com>
2020-08-25 12:20:38 -0700

    Add simple local package with extras.

---
+++ testdata/test_package_specifier/local_extras/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="repeatme",
+    version=0.1,
+    description="Repeat arguments.",
+    packages=["repeatme"],
+    extras_require={"cow": ["pycowsay==0.0.0.1"]},
+    entry_points={"console_scripts": ["repeatme=repeatme.main:main"]},
+)
