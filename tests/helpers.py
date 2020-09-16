0bceb36bca8c0936dbc8fd56ccdbf4b37fc53b42
Matthew Clapp <itsayellow+dev@gmail.com>
2020-09-16 16:48:00 -0700

    Add mock legacy venv support.

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,10 +1,34 @@
-from shutil import which
+import json
+import os
 import subprocess
 import sys
-from typing import List
+from pathlib import Path
+from shutil import which
+from typing import Any, Dict, List, Optional
 from unittest import mock
 
-from pipx import main
+from pipx import constants, main, pipx_metadata_file
+
+MOCK_PIPXMETADATA_0_1: Dict[str, Any] = {
+    "main_package": None,
+    "python_version": None,
+    "venv_args": [],
+    "injected_packages": {},
+    "pipx_metadata_version": "0.1",
+}
+
+MOCK_PACKAGE_INFO_0_1: Dict[str, Any] = {
+    "package": None,
+    "package_or_url": None,
+    "pip_args": [],
+    "include_dependencies": False,
+    "include_apps": True,
+    "apps": [],
+    "app_paths": [],
+    "apps_of_dependencies": [],
+    "app_paths_of_dependencies": {},
+    "package_version": "",
+}
 
 
 def assert_not_in_virtualenv():
@@ -16,7 +40,7 @@ def run_pipx_cli(pipx_args: List[str]):
         return main.cli()
 
 
-def which_python(python_exe):
+def which_python(python_exe: str) -> Optional[str]:
     try:
         pyenv_which = subprocess.run(
             ["pyenv", "which", python_exe],
@@ -32,3 +56,61 @@ def which_python(python_exe):
     else:
         # pyenv on system, but pyenv has no path to python_exe
         return None
+
+
+def mock_legacy_package_info(
+    modern_package_info: Dict[str, Any], metadata_version: str
+) -> Dict[str, Any]:
+    if metadata_version == "0.1":
+        mock_package_info_template = MOCK_PACKAGE_INFO_0_1
+    else:
+        assert False
+
+    mock_package_info = {}
+    for key in mock_package_info_template:
+        mock_package_info[key] = modern_package_info[key]
+
+    return mock_package_info
+
+
+def mock_legacy_venv(venv_name: str, metadata_version: Optional[str] = None) -> None:
+    venv_dir = Path(constants.PIPX_LOCAL_VENVS) / venv_name
+
+    if metadata_version is None:
+        os.remove(venv_dir / "pipx_metadata.json")
+        return
+
+    modern_metadata = pipx_metadata_file.PipxMetadata(venv_dir).to_dict()
+
+    if metadata_version == "0.1":
+        mock_pipx_metadata_template = MOCK_PIPXMETADATA_0_1
+    else:
+        assert False
+
+    # Convert to mock old metadata
+    mock_pipx_metadata = {}
+    for key in mock_pipx_metadata_template:
+        if key == "main_package":
+            mock_pipx_metadata[key] = mock_legacy_package_info(
+                modern_metadata[key], metadata_version=metadata_version
+            )
+        if key == "injected_packages":
+            for injected in modern_metadata[key]:
+                mock_pipx_metadata[key][injected] = mock_legacy_package_info(
+                    modern_metadata[key][injected], metadata_version=metadata_version
+                )
+        else:
+            mock_pipx_metadata[key] = modern_metadata[key]
+    mock_pipx_metadata["pipx_metadata_version"] = mock_pipx_metadata_template[
+        "pipx_metadata_version"
+    ]
+
+    # replicate pipx_metadata_file.PipxMetadata.write()
+    with open(venv_dir / "pipx_metadata.json", "w") as pipx_metadata_fh:
+        json.dump(
+            mock_pipx_metadata,
+            pipx_metadata_fh,
+            indent=4,
+            sort_keys=True,
+            cls=pipx_metadata_file.JsonEncoderHandlesPath,
+        )
