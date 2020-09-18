6d1469a6fe7a83e514a7d90a60f5725494038a99
Matthew Clapp <itsayellow+dev@gmail.com>
2020-09-18 11:36:16 -0700

    Add suffix venv to reinstall-all tests.

--- tests/test_reinstall_all.py
+++ tests/test_reinstall_all.py
@@ -15,3 +15,17 @@ def test_reinstall_all_legacy_venv(pipx_temp_env, capsys, metadata_version):
     assert not run_pipx_cli(["install", "pycowsay"])
     mock_legacy_venv("pycowsay", metadata_version=metadata_version)
     assert not run_pipx_cli(["reinstall-all", "--python", sys.executable])
+
+
+def test_reinstall_all_suffix(pipx_temp_env, capsys):
+    suffix = "_x"
+    assert not run_pipx_cli(["install", "pycowsay", f"--suffix={suffix}"])
+    assert not run_pipx_cli(["reinstall-all", "--python", sys.executable])
+
+
+@pytest.mark.parametrize("metadata_version", [None, "0.1"])
+def test_reinstall_all_suffix_legacy_venv(pipx_temp_env, capsys, metadata_version):
+    suffix = "_x"
+    assert not run_pipx_cli(["install", "pycowsay", f"--suffix={suffix}"])
+    mock_legacy_venv(f"pycowsay{suffix}", metadata_version=metadata_version)
+    assert not run_pipx_cli(["reinstall-all", "--python", sys.executable])
