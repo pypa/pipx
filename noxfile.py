db2ba54e8c9a31d55ffe2853aea7097546f54c13
Matthew Clapp <itsayellow+dev@gmail.com>
2020-09-29 21:12:26 -0700

    Remove comment note about cs01 nox.

--- noxfile.py
+++ noxfile.py
@@ -4,15 +4,6 @@ from pathlib import Path
 
 import nox  # type: ignore
 
-# NOTE: these tests require nox to create virtual environments
-# with venv. nox currently uses virtualenv. pipx
-# uses a fork of nox at https://github.com/cs01/nox
-# on the branch cs01/use-venv
-# To invoke nox for pipx, use:
-# pipx run --spec=git+https://github.com/cs01/nox.git@2ba8984a nox
-# until this is fixed in nox. See
-# https://github.com/theacodes/nox/issues/199
-
 python = ["3.6", "3.7", "3.8"]
 
 if sys.platform == "win32":
