bbc85456caa19d7d11cba8ef03011c982d0a0af1
Matthew Clapp <itsayellow+dev@gmail.com>
2020-10-03 19:26:17 -0700

    Update prebuild packages and use pip wheel to build.

--- noxfile.py
+++ noxfile.py
@@ -23,8 +23,8 @@ lint_dependencies = [
     "check-manifest",
     "packaging>=20.0",
 ]
-# Packages that need to be compiled and need an intact system PATH on macOS
-macos_prebuild_packages = ["regex", "argon2-cffi"]
+# Packages that need an intact system PATH to compile on macOS
+macos_prebuild_packages = ["argon2-cffi"]
 
 
 @nox.session(python=python)
@@ -32,9 +32,10 @@ def tests(session):
     if sys.platform == "darwin":
         # For macOS, we need /usr/bin in PATH to compile some packages,
         #   but pytest setup clears PATH.  So we pre-build some wheels first.
+        #   They will end up in the pip cache.
         session.install("wheel")
         for prebuild_package in macos_prebuild_packages:
-            session.install(prebuild_package)
+            session.run("pip", "wheel", prebuild_package)
     session.install("-e", ".", "pytest", "pytest-cov")
     tests = session.posargs or ["tests"]
     session.run(
