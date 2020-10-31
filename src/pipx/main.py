cfb03635abe5e9ba308edaaed7eb69655ffe8db0
Andrew Grangaard <andrew.grangaard@kernel.co>
2020-10-31 14:45:37 -0700

    Alignment for env vars help
    - Set indent to match argparse options
    - remove colons after each env var
    - Standardize the description with matching "Overrides"

--- src/pipx/main.py
+++ src/pipx/main.py
@@ -63,12 +63,12 @@ PIPX_DESCRIPTION += "\n"
 PIPX_DESCRIPTION += indented_wrap(
     """
         optional environment variables:
-          PIPX_HOME:           Overrides default pipx location. Virtual Environments will be installed to $PIPX_HOME/venvs.
-          PIPX_BIN_DIR:        Overrides location of app installations. Apps are symlinked or copied here.
-          USE_EMOJI:           Override emoji behavior. Default value varies based on platform.
-          PIPX_DEFAULT_PYTHON: Overrides default python used for commands.
+          PIPX_HOME             Overrides default pipx location. Virtual Environments will be installed to $PIPX_HOME/venvs.
+          PIPX_BIN_DIR          Overrides location of app installations. Apps are symlinked or copied here.
+          USE_EMOJI             Overrides emoji behavior. Default value varies based on platform.
+          PIPX_DEFAULT_PYTHON   Overrides default python used for commands.
     """,
-    " " * 23,  # magic number 23 matches the indent of the env descriptions.
+    " " * 24,  # match the indent of argparse options
 )
 
 DOC_DEFAULT_PYTHON = os.getenv("PIPX__DOC_DEFAULT_PYTHON", DEFAULT_PYTHON)
