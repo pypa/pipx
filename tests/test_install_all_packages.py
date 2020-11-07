5dfd248a17cb37da48b78752fb8d68cdf20a14e8
Matthew Clapp <itsayellow+dev@gmail.com>
2020-11-07 10:26:24 -0800

    Refactor, add test_error_fh, add test_deps_all_packages.

--- tests/test_install_all_packages.py
+++ tests/test_install_all_packages.py
@@ -1,3 +1,4 @@
+import io
 import os
 import re
 import subprocess
@@ -13,15 +14,81 @@ from package_info import PKG
 
 REPORTS_DIR = "./reports"
 REPORT_FILENAME_ROOT = "test_install_all_packages"
+# "package_name, package_spec",
+PACKAGE_PARAMETRIZE_LIST = [
+    ("ansible", PKG["ansible"]["spec"]),
+    ("awscli", PKG["awscli"]["spec"]),
+    ("b2", PKG["b2"]["spec"]),
+    ("beancount", PKG["beancount"]["spec"]),
+    ("beets", PKG["beets"]["spec"]),
+    ("black", PKG["black"]["spec"]),
+    ("cactus", PKG["cactus"]["spec"]),
+    ("chert", PKG["chert"]["spec"]),
+    ("cloudtoken", PKG["cloudtoken"]["spec"]),
+    ("coala", PKG["coala"]["spec"]),
+    ("cookiecutter", PKG["cookiecutter"]["spec"]),
+    ("cython", PKG["cython"]["spec"]),
+    ("datasette", PKG["datasette"]["spec"]),
+    ("diffoscope", PKG["diffoscope"]["spec"]),
+    ("doc2dash", PKG["doc2dash"]["spec"]),
+    ("doitlive", PKG["doitlive"]["spec"]),
+    ("gdbgui", PKG["gdbgui"]["spec"]),
+    ("gns3-gui", PKG["gns3-gui"]["spec"]),
+    ("grow", PKG["grow"]["spec"]),
+    ("guake", PKG["guake"]["spec"]),
+    ("gunicorn", PKG["gunicorn"]["spec"]),
+    ("howdoi", PKG["howdoi"]["spec"]),
+    ("httpie", PKG["httpie"]["spec"]),
+    ("hyde", PKG["hyde"]["spec"]),
+    ("ipython", PKG["ipython"]["spec"]),
+    ("isort", PKG["isort"]["spec"]),
+    ("jaraco-financial", PKG["jaraco-financial"]["spec"]),
+    ("kaggle", PKG["kaggle"]["spec"]),
+    ("kibitzr", PKG["kibitzr"]["spec"]),
+    ("klaus", PKG["klaus"]["spec"]),
+    ("kolibri", PKG["kolibri"]["spec"]),
+    ("lektor", PKG["lektor"]["spec"]),
+    ("localstack", PKG["localstack"]["spec"]),
+    ("mackup", PKG["mackup"]["spec"]),
+    ("magic-wormhole", PKG["magic-wormhole"]["spec"]),
+    ("mayan-edms", PKG["mayan-edms"]["spec"]),
+    ("mkdocs", PKG["mkdocs"]["spec"]),
+    ("mycli", PKG["mycli"]["spec"]),
+    ("nikola", PKG["nikola"]["spec"]),
+    ("nox", PKG["nox"]["spec"]),
+    ("pelican", PKG["pelican"]["spec"]),
+    ("platformio", PKG["platformio"]["spec"]),
+    ("ppci", PKG["ppci"]["spec"]),
+    ("prosopopee", PKG["prosopopee"]["spec"]),
+    ("ptpython", PKG["ptpython"]["spec"]),
+    ("pycowsay", PKG["pycowsay"]["spec"]),
+    ("pylint", PKG["pylint"]["spec"]),
+    ("retext", PKG["retext"]["spec"]),
+    ("robotframework", PKG["robotframework"]["spec"]),
+    ("shell-functools", PKG["shell-functools"]["spec"]),
+    ("speedtest-cli", PKG["speedtest-cli"]["spec"]),
+    ("sphinx", PKG["sphinx"]["spec"]),
+    ("sqlmap", PKG["sqlmap"]["spec"]),
+    ("streamlink", PKG["streamlink"]["spec"]),
+    ("taguette", PKG["taguette"]["spec"]),
+    ("term2048", PKG["term2048"]["spec"]),
+    ("tox-ini-fmt", PKG["tox-ini-fmt"]["spec"]),
+    ("visidata", PKG["visidata"]["spec"]),
+    ("vulture", PKG["vulture"]["spec"]),
+    ("weblate", PKG["weblate"]["spec"]),
+    ("youtube-dl", PKG["youtube-dl"]["spec"]),
+    ("zeo", PKG["zeo"]["spec"]),
+]
 
 
 @pytest.fixture(scope="module")
 def module_globals():
     return {
         "test_start": 0,
-        "error_report_path": Path("./"),
+        "error_path": Path("."),
         "report_path": Path("."),
         "install_data": {},
+        "doing_dep_test": False,
     }
 
 
@@ -29,8 +96,13 @@ def pip_cache_purge():
     return subprocess.run([sys.executable, "-m", "pip", "cache", "purge"])
 
 
-def verify_installed_apps(captured_outerr, package_name):
-    package_apps = PKG[package_name]["apps"]
+def verify_installed_apps(captured_outerr, package_name, test_error_fh, deps=False):
+    if deps:
+        package_apps = (
+            PKG[package_name]["apps"] + PKG[package_name]["apps_of_dependencies"]
+        )
+    else:
+        package_apps = PKG[package_name]["apps"]
 
     reported_apps_re = re.search(
         r"These apps are now globally available(.+)", captured_outerr.out, re.DOTALL
@@ -41,27 +113,31 @@ def verify_installed_apps(captured_outerr, package_name):
         ]
         if set(reported_apps) != set(package_apps):
             app_success = False
-            print("verify_install: REPORTED APPS DO NOT MATCH PACKAGE")
-            print(f"pipx reported apps: {reported_apps}")
-            print(f" true package apps: {package_apps}")
+            print(
+                "verify_install: REPORTED APPS DO NOT MATCH PACKAGE", file=test_error_fh
+            )
+            print(f"pipx reported apps: {reported_apps}", file=test_error_fh)
+            print(f" true package apps: {package_apps}", file=test_error_fh)
         else:
             app_success = True
     else:
         app_success = False
-        print("verify_install: APPS TESTING ERROR")
+        print("verify_install: APPS TESTING ERROR", file=test_error_fh)
 
     return app_success
 
 
-def verify_install(captured_outerr, caplog, package_name):
+def verify_install(captured_outerr, caplog, package_name, test_error_fh, deps=False):
     caplog_problem = False
     install_success = f"installed package {package_name}" in captured_outerr.out
     for record in caplog.records:
         if "⚠️" in record.message or "WARNING" in record.message:
             caplog_problem = True
-            print("verify_install: WARNINGS IN CAPLOG")
+            print("verify_install: WARNINGS IN CAPLOG", file=test_error_fh)
     if install_success and PKG[package_name].get("apps", None) is not None:
-        app_success = verify_installed_apps(captured_outerr, package_name)
+        app_success = verify_installed_apps(
+            captured_outerr, package_name, test_error_fh, deps=deps
+        )
     else:
         app_success = True
 
@@ -69,10 +145,10 @@ def verify_install(captured_outerr, caplog, package_name):
 
 
 def print_error_report(
-    error_report_path, command_captured, test_captured, package_spec, header
+    error_path, command_captured, test_error_fh, package_spec, header
 ):
     py_version_str = f"Python {sys.version_info[0]}.{sys.version_info[1]}"
-    with error_report_path.open("a") as error_fh:
+    with error_path.open("a") as error_fh:
         print("\n\n", file=error_fh)
         print("=" * 76, file=error_fh)
         print(
@@ -87,7 +163,7 @@ def print_error_report(
         print(command_captured.err, end="", file=error_fh)
         print("\n\nTEST ERRORS:", file=error_fh)
         print("-" * 76, file=error_fh)
-        print(test_captured.out, end="", file=error_fh)
+        print(test_error_fh.get_value(), end="", file=error_fh)
 
 
 def install_package(
@@ -98,21 +174,27 @@ def install_package(
     caplog,
     package_spec,
     package_name="",
+    deps=False,
 ):
     orig_path = os.getenv("PATH_ORIG")
 
+    test_error_fh = io.StringIO()
     install_data = module_globals["install_data"]
     install_data[package_spec] = {}
     if not package_name:
         package_name = package_spec
 
     start_time = time.time()
-    run_pipx_cli(["install", package_spec, "--verbose"])
+    run_pipx_cli(
+        ["install", package_spec, "--verbose"] + (["--include-deps"] if deps else [])
+    )
     elapsed_time_clear = time.time() - start_time
     elapsed_time_sys = 0
 
     captured_clear_path = capfd.readouterr()
-    install_success = verify_install(captured_clear_path, caplog, package_name)
+    install_success = verify_install(
+        captured_clear_path, caplog, package_name, test_error_fh, deps=deps
+    )
 
     if install_success:
         install_data[package_spec]["clear_path_ok"] = True
@@ -129,21 +211,20 @@ def install_package(
         start_time = time.time()
         run_pipx_cli(["install", package_spec, "--verbose"])
         elapsed_time_sys = time.time() - start_time
-
         captured_sys_path = capfd.readouterr()
+
         install_success_orig_path = verify_install(
-            captured_sys_path, caplog, package_name
+            captured_sys_path, caplog, package_name, test_error_fh, deps=deps
         )
-        captured_verify_errors = capfd.readouterr()
 
         if install_success_orig_path:
             install_data[package_spec]["sys_path_ok"] = True
         else:
             install_data[package_spec]["sys_path_ok"] = False
             print_error_report(
-                module_globals["error_report_path"],
+                module_globals["error_path"],
                 captured_sys_path,
-                captured_verify_errors,
+                test_error_fh,
                 package_spec,
                 "sys PATH",
             )
@@ -175,13 +256,18 @@ def start_end_report(module_globals):
 
     reports_path = Path(REPORTS_DIR)
     reports_path.mkdir(exist_ok=True, parents=True)
+
+    if module_globals["doing_dep_test"]:
+        deps = "_deps_"
+    else:
+        deps = "_"
     module_globals["report_path"] = (
         reports_path
-        / f"./{REPORT_FILENAME_ROOT}_report_{sys.platform}_{py_version}_{date_string}.txt"
+        / f"./{REPORT_FILENAME_ROOT}{deps}report_{sys.platform}_{py_version}_{date_string}.txt"
     )
-    module_globals["error_report_path"] = (
+    module_globals["error_path"] = (
         reports_path
-        / f"./{REPORT_FILENAME_ROOT}_errors_{sys.platform}_{py_version}_{date_string}.txt"
+        / f"./{REPORT_FILENAME_ROOT}{deps}errors_{sys.platform}_{py_version}_{date_string}.txt"
     )
 
     install_data = module_globals["install_data"]
@@ -222,73 +308,7 @@ def start_end_report(module_globals):
         print(f"Elapsed: {el_datetime}", file=report_fh)
 
 
-@pytest.mark.parametrize(
-    "package_name, package_spec",
-    [
-        ("ansible", PKG["ansible"]["spec"]),
-        ("awscli", PKG["awscli"]["spec"]),
-        ("b2", PKG["b2"]["spec"]),
-        ("beancount", PKG["beancount"]["spec"]),
-        ("beets", PKG["beets"]["spec"]),
-        ("black", PKG["black"]["spec"]),
-        ("cactus", PKG["cactus"]["spec"]),
-        ("chert", PKG["chert"]["spec"]),
-        ("cloudtoken", PKG["cloudtoken"]["spec"]),
-        ("coala", PKG["coala"]["spec"]),
-        ("cookiecutter", PKG["cookiecutter"]["spec"]),
-        ("cython", PKG["cython"]["spec"]),
-        ("datasette", PKG["datasette"]["spec"]),
-        ("diffoscope", PKG["diffoscope"]["spec"]),
-        ("doc2dash", PKG["doc2dash"]["spec"]),
-        ("doitlive", PKG["doitlive"]["spec"]),
-        ("gdbgui", PKG["gdbgui"]["spec"]),
-        ("gns3-gui", PKG["gns3-gui"]["spec"]),
-        ("grow", PKG["grow"]["spec"]),
-        ("guake", PKG["guake"]["spec"]),
-        ("gunicorn", PKG["gunicorn"]["spec"]),
-        ("howdoi", PKG["howdoi"]["spec"]),
-        ("httpie", PKG["httpie"]["spec"]),
-        ("hyde", PKG["hyde"]["spec"]),
-        ("ipython", PKG["ipython"]["spec"]),
-        ("isort", PKG["isort"]["spec"]),
-        ("jaraco-financial", PKG["jaraco-financial"]["spec"]),
-        ("kaggle", PKG["kaggle"]["spec"]),
-        ("kibitzr", PKG["kibitzr"]["spec"]),
-        ("klaus", PKG["klaus"]["spec"]),
-        ("kolibri", PKG["kolibri"]["spec"]),
-        ("lektor", PKG["lektor"]["spec"]),
-        ("localstack", PKG["localstack"]["spec"]),
-        ("mackup", PKG["mackup"]["spec"]),
-        ("magic-wormhole", PKG["magic-wormhole"]["spec"]),
-        ("mayan-edms", PKG["mayan-edms"]["spec"]),
-        ("mkdocs", PKG["mkdocs"]["spec"]),
-        ("mycli", PKG["mycli"]["spec"]),
-        ("nikola", PKG["nikola"]["spec"]),
-        ("nox", PKG["nox"]["spec"]),
-        ("pelican", PKG["pelican"]["spec"]),
-        ("platformio", PKG["platformio"]["spec"]),
-        ("ppci", PKG["ppci"]["spec"]),
-        ("prosopopee", PKG["prosopopee"]["spec"]),
-        ("ptpython", PKG["ptpython"]["spec"]),
-        ("pycowsay", PKG["pycowsay"]["spec"]),
-        ("pylint", PKG["pylint"]["spec"]),
-        ("retext", PKG["retext"]["spec"]),
-        ("robotframework", PKG["robotframework"]["spec"]),
-        ("shell-functools", PKG["shell-functools"]["spec"]),
-        ("speedtest-cli", PKG["speedtest-cli"]["spec"]),
-        ("sphinx", PKG["sphinx"]["spec"]),
-        ("sqlmap", PKG["sqlmap"]["spec"]),
-        ("streamlink", PKG["streamlink"]["spec"]),
-        ("taguette", PKG["taguette"]["spec"]),
-        ("term2048", PKG["term2048"]["spec"]),
-        ("tox-ini-fmt", PKG["tox-ini-fmt"]["spec"]),
-        ("visidata", PKG["visidata"]["spec"]),
-        ("vulture", PKG["vulture"]["spec"]),
-        ("weblate", PKG["weblate"]["spec"]),
-        ("youtube-dl", PKG["youtube-dl"]["spec"]),
-        ("zeo", PKG["zeo"]["spec"]),
-    ],
-)
+@pytest.mark.parametrize("package_name, package_spec", PACKAGE_PARAMETRIZE_LIST)
 @pytest.mark.all_packages
 def test_all_packages(
     module_globals,
@@ -301,6 +321,7 @@ def test_all_packages(
     package_spec,
 ):
     pip_cache_purge()
+    module_globals["doing_dep_test"] = False
     install_package(
         module_globals,
         monkeypatch,
@@ -309,4 +330,31 @@ def test_all_packages(
         caplog,
         package_spec,
         package_name,
+        deps=False,
+    )
+
+
+@pytest.mark.parametrize("package_name, package_spec", PACKAGE_PARAMETRIZE_LIST)
+@pytest.mark.all_packages
+def test_deps_all_packages(
+    module_globals,
+    start_end_report,
+    monkeypatch,
+    capfd,
+    pipx_temp_env,
+    caplog,
+    package_name,
+    package_spec,
+):
+    module_globals["doing_dep_test"] = True
+    pip_cache_purge()
+    install_package(
+        module_globals,
+        monkeypatch,
+        capfd,
+        pipx_temp_env,
+        caplog,
+        package_spec,
+        package_name,
+        deps=True,
     )
