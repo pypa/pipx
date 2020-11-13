df3bcc51544ba94ba17d2cc76d834d93d5655c73
Tzu-ping Chung <uranusjr@gmail.com>
2020-11-14 05:11:05 +0800

    Convert pipx to use PEP 517

--- noxfile.py
+++ noxfile.py
@@ -94,11 +94,10 @@ def develop(session):
 
 @nox.session(python="3.8")
 def build(session):
-    session.install("setuptools")
-    session.install("wheel")
+    session.install("build")
     session.install("twine")
     session.run("rm", "-rf", "dist", "build", external=True)
-    session.run("python", "setup.py", "--quiet", "sdist", "bdist_wheel")
+    session.run("python", "-m", "build")
 
 
 def has_changes():
