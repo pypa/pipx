79cee740ac58ef9a0bf6ad934b91148577f090f0
Matthew Clapp <itsayellow+dev@gmail.com>
2020-11-14 22:37:31 -0800

    Make run_pip return int.

--- src/pipx/commands/run_pip.py
+++ src/pipx/commands/run_pip.py
@@ -5,7 +5,7 @@ from pipx.util import PipxError
 from pipx.venv import Venv
 
 
-def run_pip(package: str, venv_dir: Path, pip_args: List[str], verbose: bool):
+def run_pip(package: str, venv_dir: Path, pip_args: List[str], verbose: bool) -> int:
     venv = Venv(venv_dir, verbose=verbose)
     if not venv.python_path.exists():
         raise PipxError(
@@ -13,3 +13,8 @@ def run_pip(package: str, venv_dir: Path, pip_args: List[str], verbose: bool):
         )
     venv.verbose = True
     venv._run_pip(pip_args)
+
+    # TODO: verify
+    # TODO: venv._run_pip() will raise PipxError for any non-zero pip error code
+    #   but we should return actual pip error code
+    return 0
