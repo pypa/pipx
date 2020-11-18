21c5917bc675e9b328d3804bda1bef981259dbf5
Matthew Clapp <itsayellow+dev@gmail.com>
2020-11-17 16:39:50 -0800

    Move duplicate code out of both if else branches.

--- tests/test_upgrade_all.py
+++ tests/test_upgrade_all.py
@@ -11,15 +11,12 @@ def test_upgrade_all(pipx_temp_env, capsys):
 
 @pytest.mark.parametrize("metadata_version", [None, "0.1"])
 def test_upgrade_all_legacy_venv(pipx_temp_env, capsys, caplog, metadata_version):
+    assert run_pipx_cli(["upgrade", "pycowsay"])
+    assert not run_pipx_cli(["install", "pycowsay"])
+    mock_legacy_venv("pycowsay", metadata_version=metadata_version)
     if metadata_version is None:
-        assert run_pipx_cli(["upgrade", "pycowsay"])
-        assert not run_pipx_cli(["install", "pycowsay"])
-        mock_legacy_venv("pycowsay", metadata_version=metadata_version)
         capsys.readouterr()
         assert run_pipx_cli(["upgrade-all"])
         assert "Error encountered when upgrading pycowsay" in caplog.text
     else:
-        assert run_pipx_cli(["upgrade", "pycowsay"])
-        assert not run_pipx_cli(["install", "pycowsay"])
-        mock_legacy_venv("pycowsay", metadata_version=metadata_version)
         assert not run_pipx_cli(["upgrade-all"])
