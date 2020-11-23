6633b778e38da0bbba7892ce17674a26b5f0ec7b
Matthew Clapp <itsayellow+dev@gmail.com>
2020-11-22 21:34:17 -0800

    Add test.

--- src/pipx/commands/upgrade.py
+++ src/pipx/commands/upgrade.py
@@ -155,7 +155,7 @@ def upgrade(
         force=force,
     )
 
-    # Any error in upgrade will raise PipxError from vevn._run_pip()
+    # Any error in upgrade will raise PipxError from venv._run_pip()
     return EXIT_CODE_OK
 
 
