0349fa558236eaac59b2a32fcb863e3b04826fbb
Matthew Clapp <itsayellow+dev@gmail.com>
2021-01-30 17:31:13 -0800

    Add more eror lines to possibly relevant.

--- src/pipx/util.py
+++ src/pipx/util.py
@@ -161,6 +161,7 @@ def subprocess_post_check(
 def analyze_pip_output(pip_stdout, pip_stderr):
     failed_lines = []
     error_lines = []
+    error2_lines = []
     capital_error_lines = []
     exception_lines = []
     exception_error_lines = []
@@ -178,11 +179,14 @@ def analyze_pip_output(pip_stdout, pip_stderr):
     for line in pip_stderr.split("\n"):
         exception_error_re = re.search(r"(Exception|Error):", line)
         exception_error2_re = re.search(r"(Exception|Error)", line)
+        error2_re = re.search(r"error:", line, re.I)
         error_re = re.search(r"^\s*(error.+)$", line, re.I)
         capital_error_re = re.search(r"Error", line)
         exception_re = re.search(r"Exception", line)
         if error_re:
             error_lines.append(line.strip())
+        if error2_re:
+            error2_lines.append(line.strip())
         if capital_error_re:
             capital_error_lines.append(line.strip())
         if exception_re:
@@ -208,6 +212,10 @@ def analyze_pip_output(pip_stdout, pip_stderr):
         print("Possibly relevant errors from pip install:", file=sys.stderr)
         for exception_error2_line in exception_error2_lines:
             print(f"    {exception_error2_line}", file=sys.stderr)
+    elif error2_lines:
+        print("Possibly relevant errors from pip install:", file=sys.stderr)
+        for error2_line in error2_lines:
+            print(f"    {error2_line}", file=sys.stderr)
 
     # TODO: remove this for final code
     # print("\nDEBUG:", file=sys.stderr)
