9a00952b21858cdb3c94cbc2057a6dca1b7a0ecb
Matthew Clapp <itsayellow+dev@gmail.com>
2021-02-07 01:07:13 -0800

    Make list packages code always sequential.

--- src/pipx/commands/list_packages.py
+++ src/pipx/commands/list_packages.py
@@ -1,6 +1,5 @@
-from functools import partial
 from pathlib import Path
-from typing import Callable, Collection, Optional
+from typing import Collection
 
 from pipx import constants
 from pipx.colors import bold
@@ -9,18 +8,11 @@ from pipx.constants import EXIT_CODE_LIST_PROBLEM, EXIT_CODE_OK, ExitCode
 from pipx.emojis import sleep
 from pipx.venv import VenvContainer
 
-Pool: Optional[Callable]
-try:
-    import multiprocessing.synchronize  # noqa: F401
-    from multiprocessing import Pool
-except ImportError:
-    Pool = None
-
 
 def list_packages(venv_container: VenvContainer, include_injected: bool) -> ExitCode:
     """Returns pipx exit code."""
-    dirs: Collection[Path] = sorted(venv_container.iter_venv_dirs())
-    if not dirs:
+    venv_dirs: Collection[Path] = sorted(venv_container.iter_venv_dirs())
+    if not venv_dirs:
         print(f"nothing has been installed with pipx {sleep}")
         return EXIT_CODE_OK
 
@@ -30,23 +22,12 @@ def list_packages(venv_container: VenvContainer, include_injected: bool) -> Exit
     venv_container.verify_shared_libs()
 
     all_venv_problems = VenvProblems()
-    if Pool:
-        p = Pool()
-        try:
-            for package_summary, venv_problems in p.map(
-                partial(get_package_summary, include_injected=include_injected), dirs
-            ):
-                print(package_summary)
-                all_venv_problems.or_(venv_problems)
-        finally:
-            p.close()
-            p.join()
-    else:
-        for package_summary, venv_problems in map(
-            partial(get_package_summary, include_injected=include_injected), dirs
-        ):
-            print(package_summary)
-            all_venv_problems.or_(venv_problems)
+    for venv_dir in venv_dirs:
+        package_summary, venv_problems = get_package_summary(
+            venv_dir, include_injected=include_injected
+        )
+        print(package_summary)
+        all_venv_problems.or_(venv_problems)
 
     if all_venv_problems.bad_venv_name:
         print(
