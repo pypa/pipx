9c48b61385b5b92e53633a5aa1835fad2f1ce8cc
Matthew Clapp <itsayellow+dev@gmail.com>
2021-03-23 23:10:21 -0700

    Fix cleanup of app binaries for venvs without pipx_metadata.json (#651)

--- tests/test_uninstall.py
+++ tests/test_uninstall.py
@@ -1,6 +1,6 @@
 import pytest  # type: ignore
 
-from helpers import mock_legacy_venv, run_pipx_cli
+from helpers import app_name, mock_legacy_venv, run_pipx_cli
 from package_info import PKG
 from pipx import constants, util
 
@@ -22,35 +22,42 @@ def test_uninstall_circular_deps(pipx_temp_env, capsys):
 
 @pytest.mark.parametrize("metadata_version", [None, "0.1"])
 def test_uninstall_legacy_venv(pipx_temp_env, capsys, metadata_version):
+    executable_path = constants.LOCAL_BIN_DIR / app_name("pycowsay")
+
     assert not run_pipx_cli(["install", "pycowsay"])
+    assert executable_path.exists()
+
     mock_legacy_venv("pycowsay", metadata_version=metadata_version)
     assert not run_pipx_cli(["uninstall", "pycowsay"])
+    # Also use is_symlink to check for broken symlink.
+    #   exists() returns False if symlink exists but target doesn't exist
+    assert not executable_path.exists() and not executable_path.is_symlink()
 
 
 def test_uninstall_suffix(pipx_temp_env, capsys):
     name = "pbr"
     suffix = "_a"
-    executable = f"{name}{suffix}{'.exe' if constants.WINDOWS else ''}"
+    executable_path = constants.LOCAL_BIN_DIR / app_name(f"{name}{suffix}")
 
     assert not run_pipx_cli(["install", "pbr", f"--suffix={suffix}"])
-    assert (constants.LOCAL_BIN_DIR / executable).exists()
+    assert executable_path.exists()
 
     assert not run_pipx_cli(["uninstall", f"{name}{suffix}"])
-    assert not (constants.LOCAL_BIN_DIR / executable).exists()
+    assert not executable_path.exists()
 
 
 @pytest.mark.parametrize("metadata_version", ["0.1"])
 def test_uninstall_suffix_legacy_venv(pipx_temp_env, capsys, metadata_version):
     name = "pbr"
     suffix = "_a"
-    executable = f"{name}{suffix}{'.exe' if constants.WINDOWS else ''}"
+    executable_path = constants.LOCAL_BIN_DIR / app_name(f"{name}{suffix}")
 
     assert not run_pipx_cli(["install", "pbr", f"--suffix={suffix}"])
     mock_legacy_venv(f"{name}{suffix}", metadata_version=metadata_version)
-    assert (constants.LOCAL_BIN_DIR / executable).exists()
+    assert executable_path.exists()
 
     assert not run_pipx_cli(["uninstall", f"{name}{suffix}"])
-    assert not (constants.LOCAL_BIN_DIR / executable).exists()
+    assert not executable_path.exists()
 
 
 def test_uninstall_with_missing_interpreter(pipx_temp_env, capsys):
