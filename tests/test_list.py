4eb65303ba2b5986ae3a2168f8c00d7b1a861bdf
Matthew Clapp <itsayellow+dev@gmail.com>
2021-03-30 18:53:57 -0700

    Implement test_list_json.

--- tests/test_list.py
+++ tests/test_list.py
@@ -1,7 +1,17 @@
+import json
+import re
+
 import pytest  # type: ignore
 
-from helpers import mock_legacy_venv, run_pipx_cli
+from helpers import (
+    assert_package_metadata,
+    create_package_info_ref,
+    mock_legacy_venv,
+    run_pipx_cli,
+)
+from package_info import PKG
 from pipx import constants, util
+from pipx.pipx_metadata_file import PackageInfo, _json_decoder_object_hook
 
 
 def test_cli(pipx_temp_env, monkeypatch, capsys):
@@ -59,3 +69,66 @@ def test_list_suffix_legacy_venv(pipx_temp_env, monkeypatch, capsys, metadata_ve
     assert not run_pipx_cli(["list"])
     captured = capsys.readouterr()
     assert f"package pycowsay 0.0.0.1 (pycowsay{suffix})," in captured.out
+
+
+def test_list_json(pipx_temp_env, capsys):
+    pipx_venvs_dir = constants.PIPX_HOME / "venvs"
+    venv_bin_dir = "Scripts" if constants.WINDOWS else "bin"
+
+    assert not run_pipx_cli(["install", PKG["pycowsay"]["spec"]])
+    assert not run_pipx_cli(["install", PKG["pylint"]["spec"]])
+    assert not run_pipx_cli(["inject", "pylint", PKG["isort"]["spec"]])
+    captured = capsys.readouterr()
+
+    assert not run_pipx_cli(["list", "--json"])
+    captured = capsys.readouterr()
+
+    assert not re.search(r"\S", captured.err)
+    json_parsed = json.loads(captured.out, object_hook=_json_decoder_object_hook)
+
+    # raises error if not valid json
+    assert sorted(json_parsed["venvs"].keys()) == ["pycowsay", "pylint"]
+
+    # pycowsay venv
+    pycowsay_package_ref = create_package_info_ref(
+        "pycowsay", "pycowsay", pipx_venvs_dir
+    )
+    assert_package_metadata(
+        PackageInfo(**json_parsed["venvs"]["pycowsay"]["metadata"]["main_package"]),
+        pycowsay_package_ref,
+    )
+    assert json_parsed["venvs"]["pycowsay"]["metadata"]["injected_packages"] == {}
+
+    # pylint venv
+    pylint_package_ref = create_package_info_ref(
+        "pylint",
+        "pylint",
+        pipx_venvs_dir,
+        **{
+            "app_paths_of_dependencies": {
+                "isort": [pipx_venvs_dir / "pylint" / venv_bin_dir / "isort"]
+            },
+        },
+    )
+    assert_package_metadata(
+        PackageInfo(**json_parsed["venvs"]["pylint"]["metadata"]["main_package"]),
+        pylint_package_ref,
+    )
+    assert sorted(
+        json_parsed["venvs"]["pylint"]["metadata"]["injected_packages"].keys()
+    ) == ["isort"]
+    isort_package_ref = create_package_info_ref(
+        "pylint", "isort", pipx_venvs_dir, include_apps=False
+    )
+    print(isort_package_ref)
+    print(
+        PackageInfo(
+            **json_parsed["venvs"]["pylint"]["metadata"]["injected_packages"]["isort"]
+        )
+    )
+    assert_package_metadata(
+        PackageInfo(
+            **json_parsed["venvs"]["pylint"]["metadata"]["injected_packages"]["isort"]
+        ),
+        isort_package_ref,
+    )
