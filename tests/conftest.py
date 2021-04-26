973f5340ce0fe9b5fbaf3f573427567c761af2b8
Matthew Clapp <itsayellow+dev@gmail.com>
2021-04-25 23:06:45 -0700

    Reuse same shared libraries during test session to speed tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,8 +28,44 @@ def pytest_configure(config):
 
 
 @pytest.fixture
-def pipx_temp_env(tmp_path, monkeypatch):
+def pipx_temp_env(tmp_path, tmp_path_factory, monkeypatch):
     """Sets up temporary paths for pipx to install into.
+
+    Shared libs are setup once per session, all other pipx dirs, constants are
+    recreated for every test function.
+
+    Also adds environment variables as necessary to make pip installations
+    seamless.
+    """
+    # temp_path_factory uses the same directory for the whole session
+    shared_dir = tmp_path_factory.mktemp("session_shareddir")
+    monkeypatch.setattr(constants, "PIPX_SHARED_LIBS", shared_dir)
+    monkeypatch.setattr(shared_libs, "shared_libs", shared_libs._SharedLibs())
+    monkeypatch.setattr(venv, "shared_libs", shared_libs.shared_libs)
+
+    home_dir = Path(tmp_path) / "subdir" / "pipxhome"
+    bin_dir = Path(tmp_path) / "otherdir" / "pipxbindir"
+
+    monkeypatch.setattr(constants, "PIPX_HOME", home_dir)
+    monkeypatch.setattr(constants, "LOCAL_BIN_DIR", bin_dir)
+    monkeypatch.setattr(constants, "PIPX_LOCAL_VENVS", home_dir / "venvs")
+    monkeypatch.setattr(constants, "PIPX_VENV_CACHEDIR", home_dir / ".cache")
+    monkeypatch.setattr(constants, "PIPX_LOG_DIR", home_dir / "logs")
+
+    # macOS needs /usr/bin in PATH to compile certain packages, but
+    #   applications in /usr/bin cause test_install.py tests to raise warnings
+    #   which make tests fail (e.g. on Github ansible apps exist in /usr/bin)
+    monkeypatch.setenv("PATH_ORIG", str(bin_dir) + os.pathsep + os.getenv("PATH"))
+    monkeypatch.setenv("PATH_TEST", str(bin_dir))
+    monkeypatch.setenv("PATH", str(bin_dir))
+
+
+@pytest.fixture
+def pipx_ultra_temp_env(tmp_path, monkeypatch):
+    """Sets up temporary paths for pipx to install into.
+
+    Fully temporary environment, every test function starts as if pipx has
+    never been run before
 
     Also adds environment variables as necessary to make pip installations
     seamless.
