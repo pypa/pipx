57f63c3b1718ef2d6358919b1111959e85c9cc31
Matthew Clapp <itsayellow+dev@gmail.com>
2021-05-15 18:07:06 -0700

    Add test_packages_support module in scripts.

---
+++ scripts/test_packages_support.py
@@ -0,0 +1,22 @@
+import platform
+import sys
+from pathlib import Path
+
+# Platform logic
+if sys.platform == "darwin":
+    PLATFORM = "macos"
+    FULL_PLATFORM = "macos" + platform.release().split(".")[0]
+elif sys.platform == "win32":
+    PLATFORM = "win"
+    FULL_PLATFORM = "win"
+else:
+    PLATFORM = "unix"
+    FULL_PLATFORM = "unix"
+
+
+def get_platform_list_path(package_list_dir_path: Path) -> Path:
+    platform_package_list_path = (
+        package_list_dir_path
+        / f"{FULL_PLATFORM}-python{sys.version_info[0]}.{sys.version_info[1]}.txt"
+    )
+    return platform_package_list_path
