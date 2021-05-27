454dd767473675c9edd2d72fd6736acfb696d865
Matthew Clapp <itsayellow+dev@gmail.com>
2021-05-27 01:32:37 -0700

    Replace pipxproject with pypa.

--- src/pipx/__init__.py
+++ src/pipx/__init__.py
@@ -3,6 +3,6 @@ import sys
 if sys.version_info < (3, 6, 0):
     sys.exit(
         "Python 3.6 or later is required. "
-        "See https://github.com/pipxproject/pipx "
+        "See https://github.com/pypa/pipx "
         "for installation instructions."
     )
