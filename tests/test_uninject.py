203f02304494a13b942629d9cd4df205ef267933
meowmeowcat <68463158+meowmeowmeowcat@users.noreply.github.com>
2022-02-22 14:26:09 +0800

    Update test

--- tests/test_uninject.py
+++ tests/test_uninject.py
@@ -1,15 +1,29 @@
 from helpers import run_pipx_cli
+from package_info import PKG
 
 
 def test_uninject_simple(pipx_temp_env, capsys):
     assert not run_pipx_cli(["install", "pycowsay"])
-    assert not run_pipx_cli(["inject", "pycowsay", "black"])
+    assert not run_pipx_cli(["inject", "pycowsay", PKG["black"]["spec"]])
     assert not run_pipx_cli(["uninject", "pycowsay", "black"])
 
 
-def test_uninject_include_apps(pipx_temp_env, capsys):
+def test_uninject_with_include_apps(pipx_temp_env, capsys):
     assert not run_pipx_cli(["install", "pycowsay"])
     assert not run_pipx_cli(
-        ["inject", "pycowsay", "black", "--include-deps", "--include-apps"]
+        ["inject", "pycowsay", PKG["black"]["spec"], "--include-deps", "--include-apps"]
     )
     assert not run_pipx_cli(["uninject", "pycowsay", "black"])
+    captured = capsys.readouterr()
+    assert "removed file" in captured.out
+
+
+def test_uninject_leave_deps(pipx_temp_env, capsys):
+    assert not run_pipx_cli(["install", "pycowsay"])
+    assert not run_pipx_cli(["inject", "pycowsay", PKG["black"]["spec"]])
+    assert not run_pipx_cli(
+        ["uninject", "pycowsay", "black", "--leave-deps", "--verbose"]
+    )
+    captured = capsys.readouterr()
+    assert "Uninjected package black from venv pycowsay" in captured.out
+    assert "Dependencies of uninstalled package:" not in captured.out
