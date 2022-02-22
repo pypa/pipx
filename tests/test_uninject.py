2a59e275cf4a8d1475f51e9210b4d90046a5eb31
meowmeowcat <68463158+meowmeowmeowcat@users.noreply.github.com>
2022-02-22 15:42:34 +0800

    Fix test

--- tests/test_uninject.py
+++ tests/test_uninject.py
@@ -8,14 +8,13 @@ def test_uninject_simple(pipx_temp_env, capsys):
     assert not run_pipx_cli(["uninject", "pycowsay", "black"])
 
 
-def test_uninject_with_include_apps(pipx_temp_env, capsys):
+def test_uninject_with_include_apps(pipx_temp_env, capsys, caplog):
     assert not run_pipx_cli(["install", "pycowsay"])
     assert not run_pipx_cli(
         ["inject", "pycowsay", PKG["black"]["spec"], "--include-deps", "--include-apps"]
     )
     assert not run_pipx_cli(["uninject", "pycowsay", "black", "--verbose"])
-    captured = capsys.readouterr()
-    assert "removed file" in captured.out
+    assert "removed file" in caplog.text
 
 
 def test_uninject_leave_deps(pipx_temp_env, capsys):
