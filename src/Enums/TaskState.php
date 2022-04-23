b025193139c85704bcd186a3c8de2f8244b7aea6
Dick van der Heiden <d.vanderheiden@vdhicts.nl>
2022-04-23 10:08:50 +0200

    Properly implement task results

---
+++ src/Enums/TaskState.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Vdhicts\Cyberfusion\ClusterApi\Enums;
+
+class TaskState
+{
+    public const STATE_PENDING = 'pending';
+    public const STATE_STARTED = 'started';
+    public const STATE_SUCCESS = 'success';
+    public const STATE_FAILURE = 'failure';
+    public const STATE_RETRY = 'retry';
+    public const STATE_REVOKED = 'revoked';
+
+    public const AVAILABLE = [
+        self::STATE_PENDING,
+        self::STATE_STARTED,
+        self::STATE_SUCCESS,
+        self::STATE_FAILURE,
+        self::STATE_RETRY,
+        self::STATE_REVOKED,
+    ];
+}
