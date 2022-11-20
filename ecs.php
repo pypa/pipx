f220bfa9d5a8d04b6c6eb4596034ea732e09632a
Dick van der Heiden <d.vanderheiden@vdhicts.nl>
2022-11-20 13:28:35 +0100

    Create ECS config

---
+++ ecs.php
@@ -0,0 +1,25 @@
+<?php
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer;
+use PhpCsFixer\Fixer\ControlStructure\NoUnneededControlParenthesesFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    $ecsConfig->sets([
+        SetList::CONTROL_STRUCTURES,
+        SetList::CLEAN_CODE,
+        SetList::PSR_12,
+    ]);
+
+    $ecsConfig->skip([
+        NoUnneededControlParenthesesFixer::class,
+        OrderedClassElementsFixer::class,
+    ]);
+};
