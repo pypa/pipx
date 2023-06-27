dd47d9d45013da0854a1cb7bc1a1c62852891310
chrysle <i.kant@posteo.de>
2023-06-27 15:03:40 +0200

    Revert "Use `MINIMUM_PYTHON_VERSION` constant in `__init.py__`"
    This reverts commit 1bd51124792dac6af7901d531ea6eba84a16445c.

--- src/pipx/__init__.py
+++ src/pipx/__init__.py
@@ -1,9 +1,8 @@
 import sys
-from constants import MINIMUM_PYTHON_VERSION
 
-if sys.version_info < tuple(int(number) for number in MINIMUM_PYTHON_VERSION.split(".")):
+if sys.version_info < (3, 8, 0):
     sys.exit(
-       f"Python {MINIMUM_PYTHON_VERSION} or later is required. "
+        "Python 3.8 or later is required. "
         "See https://github.com/pypa/pipx "
         "for installation instructions."
     )
