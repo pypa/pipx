a2d9e09322f3581a57af7c48e14759b17d6f2eec
Carlos Pereira Atencio <4189262+carlosperate@users.noreply.github.com>
2023-11-11 13:20:54 +0000

    Remove unused statement (an accidental stand-alone string). (#1105)

--- scripts/list_test_packages.py
+++ scripts/list_test_packages.py
@@ -146,7 +146,6 @@ def create_test_packages_list(
         all_packages.append(f"{package_name}=={package_version}")
 
     with platform_package_list_path.open("w") as package_list_fh:
-        "scripts/list_test_packages.py",
         for package in sorted(all_packages):
             print(package, file=package_list_fh)
 
