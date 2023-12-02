9cab360b427513aac2d4d4469a10ad7fd859d8f2
Jarek Potiuk <jarek@potiuk.com>
2023-12-02 19:59:06 +0100

    Fix combining of `--editable` and `--force` flag
    The 1.3.0 version of `pipx` (with #933) introduced a but where
    the `--force-reinstall` flag caused failure when also
    the `--editable` flag was used - because it was adding the
    `--force-reinstall` flag after `--editable` one (and `--editable`
    flag expects url/path to the Python package to install to follow it.
    
    The change fixes it by adding the `--force-reinstall` flag at the
    beginning rather than at the end of arguments to avoid this
    kind of problem also in case other flags might have similar problem.
    
    Fixes: #1122

--- tests/test_install.py
+++ tests/test_install.py
@@ -13,6 +13,8 @@ from pipx import constants
 
 TEST_DATA_PATH = "./testdata/test_package_specifier"
 
+SOURCE_DIR = Path(__file__).parent.parent.resolve()
+
 
 def test_help_text(monkeypatch, capsys):
     mock_exit = mock.Mock(side_effect=ValueError("raised in test to exit early"))
@@ -283,6 +285,17 @@ def test_force_install_changes(pipx_temp_env, capsys):
     assert not run_pipx_cli(["install", "nox", "--force"])
     captured = capsys.readouterr()
     assert "2022.1.7" not in captured.out
+
+
+def test_force_install_changes_editable(pipx_temp_env, capsys):
+    empty_project_path_as_string = (SOURCE_DIR / "testdata" / "empty_project").as_posix()
+    assert not run_pipx_cli(["install", "--editable", empty_project_path_as_string])
+    captured = capsys.readouterr()
+    assert "empty-project" in captured.out
+
+    assert not run_pipx_cli(["install", "--editable", empty_project_path_as_string, "--force"])
+    captured = capsys.readouterr()
+    assert "Installing to existing venv 'empty-project'" in captured.out
 
 
 def test_preinstall(pipx_temp_env, caplog):
