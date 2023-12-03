fb5c6016e9bc240fd3fef12f5d24555b0b0d64c5
Johan Widén <j.e.widen@gmail.com>
2023-12-03 11:39:12 +0100

    Broke long line into shorter lines

--- src/pipx/main.py
+++ src/pipx/main.py
@@ -321,7 +321,8 @@ def add_pip_venv_args(parser: argparse.ArgumentParser) -> None:
         "--pip-args",
         help=(
             "Arbitrary pip arguments to pass directly to pip install/upgrade commands. "
-            "Example: pipx install --index-url https://test.pypi.org/simple/ --pip-args='--extra-index-url https://pypi.org/simple/' some-package"
+            "Example: pipx install --index-url https://test.pypi.org/simple/ "
+            "--pip-args='--extra-index-url https://pypi.org/simple/' some-package"
         ),
     )
 
