6918c4de5898d65807a2255eed43b6f5a6cf1a21
Robert <49005401+Gitznik@users.noreply.github.com>
2024-01-14 22:14:12 +0100

    Potential fix for pylauncher with python version (#1186)
    * Potential fix for pylauncher with python version
    
    * Validate provided python version at startup
    
    * Add hint when version looks like path but is not found
    
    * add tests
    
    * fix interpreter resolution test on windows
    
    * remove redundant error handling from install command
    
    * Reduce complexity for checking path
    
    * Fix changelog
    
    * Add news fragment
    
    * Split doc and fix part into seperate fragments
    
    ---------
    
    Co-authored-by: Bernát Gábor <bgabor8@bloomberg.net>

--- src/pipx/interpreter.py
+++ src/pipx/interpreter.py
@@ -1,11 +1,15 @@
+import logging
 import os
 import shutil
 import subprocess
 import sys
+from pathlib import Path
 from typing import Optional
 
 from pipx.constants import WINDOWS
 from pipx.util import PipxError
+
+logger = logging.getLogger(__name__)
 
 
 def has_venv() -> bool:
@@ -15,6 +19,46 @@ def has_venv() -> bool:
         return True
     except ImportError:
         return False
+
+
+class InterpreterResolutionError(PipxError):
+    def __init__(self, source: str, version: str, wrap_message: bool = True):
+        self.source = source
+        self.version = version
+        potentially_path = "/" in version
+        potentially_pylauncher = "python" not in version and not potentially_path
+
+        message = (
+            f"No executable for the provided Python version '{version}' found in {source}."
+            " Please make sure the provided version is "
+        )
+        if source == "py launcher":
+            message += "listed when running `py --list`."
+        if source == "PATH":
+            message += "on your PATH or the file path is valid. "
+            if potentially_path:
+                message += "The provided version looks like a path, but no executable was found there."
+            if potentially_pylauncher:
+                message += (
+                    "The provided version looks like a version for Python Launcher, " "but `py` was not found on PATH."
+                )
+        super().__init__(message, wrap_message)
+
+
+def find_python_interpreter(python_version: str) -> str:
+    if Path(python_version).is_file():
+        return python_version
+
+    try:
+        py_executable = find_py_launcher_python(python_version)
+        if py_executable:
+            return py_executable
+    except (subprocess.CalledProcessError, FileNotFoundError) as e:
+        raise InterpreterResolutionError(source="py launcher", version=python_version) from e
+
+    if shutil.which(python_version):
+        return python_version
+    raise InterpreterResolutionError(source="PATH", version=python_version)
 
 
 # The following code was copied from https://github.com/uranusjr/pipx-standalone
@@ -30,8 +74,14 @@ def has_venv() -> bool:
 def find_py_launcher_python(python_version: Optional[str] = None) -> Optional[str]:
     py = shutil.which("py")
     if py and python_version:
+        python_semver = python_version
+        if python_version.startswith("python"):
+            logging.warn(
+                "Removing `python` from the start of the version, as pylauncher just expects the semantic version"
+            )
+            python_semver = python_semver.lstrip("python")
         py = subprocess.run(
-            [py, f"-{python_version}", "-c", "import sys; print(sys.executable)"],
+            [py, f"-{python_semver}", "-c", "import sys; print(sys.executable)"],
             capture_output=True,
             text=True,
             check=True,
