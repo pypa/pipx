7e7efcd6bdc3a3408578c0d18d50ae4ef78ec2c8
eve Glitch <eglitch@cyberfusion.nl>
2024-05-09 10:42:48 +0200

    fix daemons styling

--- src/Endpoints/Daemons.php
+++ src/Endpoints/Daemons.php
@@ -10,146 +10,146 @@ use Cyberfusion\ClusterApi\Support\ListFilter;
 
 class Daemons extends Endpoint
 {
-    /**
-     * @throws RequestException
-     */
-    public function list(?ListFilter $filter = null): Response
-    {
-        if (!$filter instanceof ListFilter) {
-            $filter = new ListFilter();
-        }
-
-        $request = (new Request())
-            ->setMethod(Request::METHOD_GET)
-            ->setUrl(sprintf('daemons?%s', $filter->toQuery()));
-
-        $response = $this
-            ->client
-            ->request($request);
-        if (!$response->isSuccess()) {
-            return $response;
-        }
-
-        return $response->setData([
-            'daemons' => array_map(
-                fn (array $data) => (new Daemon())->fromArray($data),
-                $response->getData()
-            ),
-        ]);
+  /**
+   * @throws RequestException
+   */
+  public function list(?ListFilter $filter = null): Response
+  {
+    if (!$filter instanceof ListFilter) {
+      $filter = new ListFilter();
     }
 
-    /**
-     * @throws RequestException
-     */
-    public function get(int $id): Response
-    {
-        $request = (new Request())
-            ->setMethod(Request::METHOD_GET)
-            ->setUrl(sprintf('daemons/%d', $id));
+    $request = (new Request())
+      ->setMethod(Request::METHOD_GET)
+      ->setUrl(sprintf('daemons?%s', $filter->toQuery()));
 
-        $response = $this
-            ->client
-            ->request($request);
-        if (!$response->isSuccess()) {
-            return $response;
-        }
-
-        return $response->setData([
-            'daemon' => (new Daemon())->fromArray($response->getData()),
-        ]);
+    $response = $this
+      ->client
+      ->request($request);
+    if (!$response->isSuccess()) {
+      return $response;
     }
 
-    /**
-     * @throws RequestException
-     */
-    public function create(Daemon $daemon): Response
-    {
-        $this->validateRequired($daemon, 'create', [
-            'name',
-            'command',
-            'nodes_ids',
-            'unix_user_id',
-        ]);
+    return $response->setData([
+      'daemons' => array_map(
+        fn (array $data) => (new Daemon())->fromArray($data),
+        $response->getData()
+      ),
+    ]);
+  }
 
-        $request = (new Request())
-            ->setMethod(Request::METHOD_POST)
-            ->setUrl('daemons')
-            ->setBody(
-                $this->filterFields($daemon->toArray(), [
-                    'name',
-                    'command',
-                    'unix_user_id',
-                    'nodes_ids',
-                ])
-            );
+  /**
+   * @throws RequestException
+   */
+  public function get(int $id): Response
+  {
+    $request = (new Request())
+      ->setMethod(Request::METHOD_GET)
+      ->setUrl(sprintf('daemons/%d', $id));
 
-        $response = $this
-            ->client
-            ->request($request);
-        if (!$response->isSuccess()) {
-            return $response;
-        }
-
-        $daemon = (new Daemon())->fromArray($response->getData());
-
-        return $response->setData([
-            'daemon' => $daemon,
-        ]);
+    $response = $this
+      ->client
+      ->request($request);
+    if (!$response->isSuccess()) {
+      return $response;
     }
 
-    /**
-     * @throws RequestException
-     */
-    public function update(Daemon $daemon): Response
-    {
-        $this->validateRequired($daemon, 'update', [
-            'name',
-            'command',
-            'unix_user_id',
-            'nodes_ids',
-            'cluster_id',
-            'id',
-        ]);
+    return $response->setData([
+      'daemon' => (new Daemon())->fromArray($response->getData()),
+    ]);
+  }
 
-        $request = (new Request())
-            ->setMethod(Request::METHOD_PUT)
-            ->setUrl(sprintf('daemons/%d', $daemon->getId()))
-            ->setBody(
-                $this->filterFields($daemon->toArray(), [
-                    'name',
-                    'command',
-                    'unix_user_id',
-                    'nodes_ids',
-                    'node_id',
-                    'id',
-                ])
-            );
+  /**
+   * @throws RequestException
+   */
+  public function create(Daemon $daemon): Response
+  {
+    $this->validateRequired($daemon, 'create', [
+      'name',
+      'command',
+      'nodes_ids',
+      'unix_user_id',
+    ]);
 
-        $response = $this
-            ->client
-            ->request($request);
-        if (!$response->isSuccess()) {
-            return $response;
-        }
+    $request = (new Request())
+      ->setMethod(Request::METHOD_POST)
+      ->setUrl('daemons')
+      ->setBody(
+        $this->filterFields($daemon->toArray(), [
+          'name',
+          'command',
+          'unix_user_id',
+          'nodes_ids',
+        ])
+      );
 
-        $daemon = (new Daemon())->fromArray($response->getData());
-
-        return $response->setData([
-            'daemon' => $daemon,
-        ]);
+    $response = $this
+      ->client
+      ->request($request);
+    if (!$response->isSuccess()) {
+      return $response;
     }
 
-    /**
-     * @throws RequestException
-     */
-    public function delete(int $id): Response
-    {
-        $request = (new Request())
-            ->setMethod(Request::METHOD_DELETE)
-            ->setUrl(sprintf('daemons/%d', $id));
+    $daemon = (new Daemon())->fromArray($response->getData());
 
-        return $this
-            ->client
-            ->request($request);
+    return $response->setData([
+      'daemon' => $daemon,
+    ]);
+  }
+
+  /**
+   * @throws RequestException
+   */
+  public function update(Daemon $daemon): Response
+  {
+    $this->validateRequired($daemon, 'update', [
+      'name',
+      'command',
+      'unix_user_id',
+      'nodes_ids',
+      'cluster_id',
+      'id',
+    ]);
+
+    $request = (new Request())
+      ->setMethod(Request::METHOD_PUT)
+      ->setUrl(sprintf('daemons/%d', $daemon->getId()))
+      ->setBody(
+        $this->filterFields($daemon->toArray(), [
+          'name',
+          'command',
+          'unix_user_id',
+          'nodes_ids',
+          'node_id',
+          'id',
+        ])
+      );
+
+    $response = $this
+      ->client
+      ->request($request);
+    if (!$response->isSuccess()) {
+      return $response;
     }
+
+    $daemon = (new Daemon())->fromArray($response->getData());
+
+    return $response->setData([
+      'daemon' => $daemon,
+    ]);
+  }
+
+  /**
+   * @throws RequestException
+   */
+  public function delete(int $id): Response
+  {
+    $request = (new Request())
+      ->setMethod(Request::METHOD_DELETE)
+      ->setUrl(sprintf('daemons/%d', $id));
+
+    return $this
+      ->client
+      ->request($request);
+  }
 }
