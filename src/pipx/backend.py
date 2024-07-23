2b0dc8fe17a5844399238bb958e1b27a3ce2f675
meowmeow <meowmeowcat1211@gmail.com>
2024-07-23 14:15:36 +0000

    Improve logic

--- src/pipx/backend.py
+++ src/pipx/backend.py
@@ -8,12 +8,14 @@ logger = logging.getLogger(__name__)
 
 
 def path_to_exec(executable: str, installer: bool = False) -> str:
+    if executable == "venv" or "pip":
+        return executable
     path = shutil.which(executable)
     if path:
         return path
     elif installer:
         logger.warning(f"{executable} not found on PATH. Falling back to pip.")
-        return "pip"
+        return ""
     else:
         logger.warning(f"{executable} not found on PATH. Falling back to venv.")
-        return "venv"
+        return ""
