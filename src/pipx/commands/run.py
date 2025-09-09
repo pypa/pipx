651c9e7e1f5f5ebe7d925ba67661b6d2eda52a8c
Pablo Correa Gómez <pablo.correa.gomez@combine.se>
2025-09-09 16:40:13 +0200

    chore(run): transform comment into docstring

--- src/pipx/commands/run.py
+++ src/pipx/commands/run.py
@@ -43,8 +43,8 @@ Available executable scripts:
 
 
 def maybe_script_content(app: str, is_path: bool) -> Optional[Union[str, Path]]:
-    # If the app is a script, return its content.
-    # Return None if it should be treated as a package name.
+    """If the app is a script, return its content.
+    Return None if it should be treated as a package name."""
 
     # Look for a local file first.
     app_path = Path(app)
