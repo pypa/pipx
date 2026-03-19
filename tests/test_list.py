491dcedce6c001b514675d5a2e7b63c7258a5f70
Vivid <absolutely.vivid@fastmail.org>
2026-03-19 20:41:15 +0000

    Test venv.list_installed_packages error handling

--- tests/test_list.py
+++ tests/test_list.py
@@ -18,8 +18,9 @@ from helpers import (
     skip_if_windows,
 )
 from package_info import PKG
-from pipx import constants, paths, shared_libs
+from pipx import constants, paths, shared_libs, venv
 from pipx.pipx_metadata_file import PackageInfo, _json_decoder_object_hook
+from pipx.util import PipxError
 
 
 def test_cli(pipx_temp_env, monkeypatch, capsys):
@@ -242,3 +243,38 @@ def test_list_pinned_packages_include_injected(pipx_temp_env, monkeypatch, capsy
     assert "nox 2023.4.22" in captured.out
     assert "pylint 3.0.4" in captured.out
     assert "black 22.8.0 (injected in venv pylint)" in captured.out
+
+
+def test_list_installed_packages_error(pipx_temp_env, monkeypatch, tmp_path):
+    called_with = None
+
+    def mockreturn(args):
+        nonlocal called_with
+        called_with = args
+
+        class MockPopen:
+            def __init__(self, args):
+                self.args = args
+                self.returncode = 1
+                self.stderr = "unit test stderr"
+
+        return MockPopen(args)
+
+    # we don't want to actually spawn a subprocess, we just want to test the
+    # error handling for a failed pip list command
+    monkeypatch.setattr(venv, "run_subprocess", mockreturn)
+    fake_venv = venv.Venv(tmp_path / "fake_venv")
+
+    with pytest.raises(PipxError) as excinfo:
+        fake_venv.list_installed_packages()
+
+    assert len(excinfo.value.args) == 1
+
+    # the pipx error wrapper inserts newlines into the output, so we need to
+    # stratgeically replace those newlines for an easier comparison
+    actual = excinfo.value.args[0].replace(",\n", ", ").replace(".\n", ". ").replace("\n", "")
+
+    expected = f"Failed to execute {called_with}. Process exited with return code 1. stderr: unit test stderr"
+
+
+    assert actual == expected
