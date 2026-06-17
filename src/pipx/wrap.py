80e531e4913fe2d31f04962292d230305de42b4f
marko1olo <96879250+marko1olo@users.noreply.github.com>
2026-06-18 00:54:24 +0400

    Fix pipx.util import cycle (#1831)

---
+++ src/pipx/wrap.py
@@ -0,0 +1,29 @@
+import shutil
+import textwrap
+
+
+def pipx_wrap(text: str, subsequent_indent: str = "", keep_newlines: bool = False) -> str:
+    """Dedent, strip, wrap to shell width. Don't break on hyphens, only spaces"""
+    minimum_width = 40
+    width = max(shutil.get_terminal_size((80, 40)).columns, minimum_width) - 2
+
+    text = textwrap.dedent(text).strip()
+    if keep_newlines:
+        return "\n".join(
+            [
+                textwrap.fill(
+                    line,
+                    width=width,
+                    subsequent_indent=subsequent_indent,
+                    break_on_hyphens=False,
+                )
+                for line in text.splitlines()
+            ]
+        )
+    else:
+        return textwrap.fill(
+            text,
+            width=width,
+            subsequent_indent=subsequent_indent,
+            break_on_hyphens=False,
+        )
